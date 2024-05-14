f7f6705221fe9e12c0c4e6a90cf2b766cb9919b7
Aakansha16 <aakansha.chouhan@idealittechno.com>
2024-05-14 13:31:30 +0530

    update work

--- src/app/components/home/list/list.component.ts
+++ src/app/components/home/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,17 @@ import { Component,OnInit} from '@angular/core';
 })
 export class ListComponent implements OnInit {
 listData:any=[]
+constructor(public router: Router){}
 ngOnInit() {
-let data=localStorage.getItem('listData');
-this.listData=data
+let data:any=localStorage.getItem('listData');
+this.listData=JSON.parse(data)
 console.log(this.listData,'listData');
 }
+deleteRecord(data:any,index:any){
+  this.listData.splice(index, 1);
 }
+redirectToEdit(data:any){
+this.router.navigate(['/dashoard/userRegistor'],{ queryParams: {type:'edit',data: JSON.stringify(data)}})
+}
+}
+
--- src/app/components/home/register-form/register-form.component.ts
+++ src/app/components/home/register-form/register-form.component.ts
@@ -1,22 +1,36 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router,ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
-  styleUrls: ['./register-form.component.scss']
+  styleUrls: ['./register-form.component.scss'],
 })
 export class RegisterFormComponent {
-  listData:any=[]
-  constructor(
-public router:Router,
-  ) {}
+  listData: any = [];
+  pageType:any
+  constructor(public router: Router,public route: ActivatedRoute) {
+  }
   model: any = {};
+  ngOnInit() {
+    this.pageType = this.route.snapshot.queryParamMap.get('type')
+    let editData:any = (this.route.snapshot.queryParamMap.get('data'))
+    if(editData){
+      this.model = JSON.parse(editData)
+    }
+    // if(editData){
+    //   this.model.userName = this.model?.userName
+    //   this.model.email = this.model?.email
+    //   this.model.phoneNumber = this.model?.phoneNumber
+    //   this.model.city = this.model?.city
+    //   console.log( this.model,' this.route.snapshot.queryParamMap.get')
+    //   }
+    }
   onSubmit(form: any) {
     console.log(form.valid, 'form.valid');
     if (form.valid) {
- this.listData.push(this.model)
-      localStorage.setItem('listData',JSON.stringify( this.listData));
+      this.listData.push(this.model);
+      localStorage.setItem('listData', JSON.stringify(this.listData));
     } else {
     }
   }
